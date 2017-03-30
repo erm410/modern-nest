4c8ab68bc89c78b6763f34381a69592401ddacaa
Ted Mitchell <erm410@gmail.com>
2017-03-29 20:45:25 -0400

    Reduce disclaimer size

--- loffle/template-parts/content-single.php
+++ loffle/template-parts/content-single.php
@@ -21,7 +21,7 @@
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
-		<p>
+		<p class="disclaimer">
 			I may receive a small compensation for products purchased through links on this page. See <a href="https://www.homemakersynonymous.com/?page_id=327#affiliate">here</a> for full info.
 		</p>
 		<?php
