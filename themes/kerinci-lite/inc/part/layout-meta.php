6f755ee44bbe39eeb30e3ea58b504739ec8a77d6
Ted Mitchell <erm410@gmail.com>
2017-11-14 23:49:07 -0500

    Remove unused themes

--- themes/kerinci-lite/inc/part/layout-meta.php
+++
@@ -1,6 +0,0 @@
-<div class="meta clearfix">  
-   <?php
-        kerinci_lite_tags();
-		kerinci_lite_date();
-    ?>
-</div>
--- themes/kerinci-lite/sidebar.php
+++
@@ -1,9 +0,0 @@
-<aside id="primary-sidebar" class="widget-area col-md-4" role="complementary">
-
-	<?php 
-	if ( is_active_sidebar( 'primary-sidebar' ) ) { 
-		dynamic_sidebar( 'primary-sidebar' );
-	}
-	?>
-
-</aside><!-- #primary-sidebar -->
